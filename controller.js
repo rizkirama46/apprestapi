3b6c0636ed87dbd4c29e27d176f06a34fed9ee5d
rizkirama46 <rizkirama46@gmail.com>
2021-09-19 17:22:23 +0700

    berhasil menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -3,6 +3,16 @@
 const respone = require('./res')
 const connection = require('./koneksi')
 
-exports.index = function(req, res){
-  respone.ok("Aplikasi REST API ku berjalan!")
+exports.index = function(req, res) {
+  respone.ok("Aplikasi REST API ku berjalan!", res)
+}
+
+exports.tampilMahasiswa = function(req, res){
+  connection.query('SELECT * FROM mahasiswa',  function(error, rows, fields) {
+    if(error) {
+      console.log(error);
+    } else {
+      respone.ok(rows, res)
+    }
+  })
 }
