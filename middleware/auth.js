0768d8fbf5e07dd6bd27a742ca56b3407b81fe91
rizkirama46 <rizkirama46@gmail.com>
2021-09-20 15:07:15 +0700

    berhasil registrasi

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const connection = require('../koneksi')
 const mysql = require('mysql')
-const md5 = require('MD5')
+const md5 = require('md5')
 const respone = require('../res')
 const jwt = require('jsonwebtoken')
 const config = require('../config/secret')
@@ -12,24 +12,24 @@ exports.registrasi = function(req, res) {
     username: req.body.username,
     email: req.body.email,
     password: md5(req.body.password),
-    role: req.body.role,
-    tanggal_daftar: new Date()
+    role: +req.body.role,
+    tanggal_daftar: req.body.tanggal_daftar
   }
 
-  const query = "SELECT email FROM ?? WHERE ??"
-  const table = ["user", "email", post.email]
+  const query = `SELECT email FROM ?? WHERE ??=?`
+  const table = ['user', 'email', post.email]
 
-  query = mysql.format(query, table)
+  const sql = mysql.format(query, table)
 
-  connection.query(query, function(error, rows) {
+  connection.query(sql, function(error, rows) {
     if(error) {
       console.log(error);
     } else {
       if(rows.length == 0) {
-        const query = "INSERT INTO ?? SET ??"
+        const query = "INSERT INTO ?? SET ?"
         const table = ["user"]
-        query = mysql.format(query, table)
-        connection.query(query, function(error, rows) {
+        const sql = mysql.format(query, table)
+        connection.query(sql, post, function(error, rows) {
           if(error) {
             console.log(error);
           } else {
@@ -37,7 +37,7 @@ exports.registrasi = function(req, res) {
           }
         })
       } else {
-        respone.ok('Email sudah terdaftar')
+        respone.ok('Email sudah terdaftar', res)
       }
     }
   })
