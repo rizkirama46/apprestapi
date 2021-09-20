b5984419cb028936ef3b1c61436ddd426d9f00a7
rizkirama46 <rizkirama46@gmail.com>
2021-09-20 15:36:46 +0700

    login user

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ const respone = require('../res')
 const jwt = require('jsonwebtoken')
 const config = require('../config/secret')
 const ip = require('ip')
+const { token } = require('morgan')
 
 //controller untuk register
 exports.registrasi = function(req, res) {
@@ -39,6 +40,60 @@ exports.registrasi = function(req, res) {
       } else {
         respone.ok('Email sudah terdaftar', res)
       }
+    }
+  })
+
+}
+
+//controller untuk login
+exports.login = function(req, res) {
+  const post = {
+    email: req.body.email,
+    password: md5(req.body.password)
+  }
+
+  const query = `SELECT * FROM ?? WHERE ??=? AND ??=?`
+  const table = ['user', 'email', post.email, 'password', post.password]
+
+  const sql = mysql.format(query, table)
+
+  connection.query(sql, function(error, rows) {
+    if(error) {
+      console.log(error);
+    } else {
+      if(rows.length == 1) {
+        const token = jwt.sign({rows}, config.secret, {
+          expiresIn: 1440
+        })
+       
+        id = rows[0].id
+
+        const data = {
+          id_user: id,
+          access_token: token,
+          ip_address: ip.address()
+        }
+
+        const query = `INSERT INTO ?? SET ?`
+        const table = ['akses_token']
+
+        const sql = mysql.format(query, table)
+
+        connection.query(sql, data, function(error, rows) {
+          if(error) {
+            console.log(error);
+          } else {
+             res.json({
+               success: true,
+               message: 'Token JWS Tergenerate!',
+               token: token,
+               currUser: data.id_user
+             });
+          }
+        })
+      } else {
+        res.json({'Error': true, "Message": "Email atau Password Salah!"});
+     }
     }
   })
 }
